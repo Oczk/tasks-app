2e1bea3c6ffbb4f8e4166eef53cef9d59c332e36
Oczk <jakub.k.oczkowski@gmail.com>
2023-02-02 14:19:30 +0100

    added TaskController with reading method and custom paging

---
+++ src/main/java/io/ocz/controller/TaskController.java
@@ -0,0 +1,34 @@
+package io.ocz.controller;
+
+import io.ocz.model.task.Task;
+import io.ocz.model.task.TaskRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@RepositoryRestController
+class TaskController {
+    private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
+    private final TaskRepository repository;
+
+    TaskController(TaskRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
+    ResponseEntity<List<Task>> readAllTasks() {
+        logger.info("Reading all tasks.");
+        return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping("/tasks")
+    ResponseEntity<List<Task>> readAllTasks(Pageable pageable) {
+        logger.info("Custom pageable");
+        return ResponseEntity.ok(repository.findAll(pageable).getContent());
+    }
+}

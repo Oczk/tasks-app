903ca325eb9ff275b7c86dbde0efd45d20722b42
Oczk <jakub.k.oczkowski@gmail.com>
2023-02-21 18:15:59 +0100

    End2End test

---
+++ src/test/java/io/ocz/TestConfiguration.java
@@ -0,0 +1,23 @@
+package io.ocz;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class TestConfiguration {
+
+    @Bean
+    @Primary
+    @Profile("!integration")
+    DataSource e2eTestDataSource() {
+        var result = new DriverManagerDataSource("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "ocz", "");
+        result.setDriverClassName("org.h2.Driver");
+
+        return result;
+    }
+}
---
+++ src/test/java/io/ocz/controller/TaskControllerE2ETest.java
@@ -0,0 +1,40 @@
+package io.ocz.controller;
+
+import io.ocz.model.Task;
+import io.ocz.model.contract.TaskRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class TaskControllerE2ETest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private TaskRepository repository;
+
+    @Test
+    void httpGet_returnsAllTasks() {
+        //given
+        repository.save(new Task("foo", LocalDateTime.now()));
+        repository.save(new Task("bar", LocalDateTime.now()));
+
+        //when
+        Task[] result = restTemplate.getForObject("http://localhost:" + port + "/tasks", Task[].class);
+
+        //then
+        assertThat(result).hasSize(2);
+    }
+}

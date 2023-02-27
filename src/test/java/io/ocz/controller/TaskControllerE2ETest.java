7028ecc10609c2aecc6733ff47306071dc253db1
Oczk <jakub.k.oczkowski@gmail.com>
2023-02-27 16:18:31 +0100

    post E2E test added

--- src/test/java/io/ocz/controller/TaskControllerE2ETest.java
+++ src/test/java/io/ocz/controller/TaskControllerE2ETest.java
@@ -7,10 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
 
 import java.time.LocalDateTime;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TaskControllerE2ETest {
@@ -31,9 +34,29 @@ class TaskControllerE2ETest {
         repository.save(new Task("bar", LocalDateTime.now()));
 
         //when
-        Task[] result = restTemplate.getForObject("http://localhost:" + port + "/tasks", Task[].class);
+        Task[] result = restTemplate.getForObject(getServerUrl(), Task[].class);
 
         //then
         assertThat(result).hasSize(2);
+    }
+
+    @Test
+    void httpPost_createNewTask() {
+        //given
+        Task task = new Task("createdWithTest", LocalDateTime.now());
+        HttpEntity<Task> request = new HttpEntity<>(task);
+
+        //when
+        ResponseEntity<Task> result = restTemplate.postForEntity(getServerUrl(), request, Task.class);
+
+        //then
+        assertNotNull(result);
+        assertNotNull(result.getBody());
+        assertNotEquals(result.getBody().getId(), 0);
+        assertEquals(task.getDescription(), result.getBody().getDescription());
+    }
+
+    private String getServerUrl() {
+        return "http://localhost:" + port + "/tasks";
     }
 }
